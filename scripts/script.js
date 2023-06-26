76517bb54d2df1133ab19b8c98fcbfe6b975741d
Fernanda <107973615+fernandaib@users.noreply.github.com>
2023-06-25 23:43:34 -0700

    COMP699
    COMP699

---
+++ scripts/script.js
@@ -0,0 +1,139 @@
+//STEP 1
+// let someMonth;
+// function theMonth(){};
+// let currentMont;
+// let summerMonth;
+// let myLibraryFunction;
+
+//STEP 2
+// // *numeric literal expression
+// 5.1
+// //string literal expression
+// 'College'
+// //Boolean literal expression
+// true
+// //null literal expression
+// null
+// //STEP 3
+// let expression = 2 * (8-3) + 5;
+// let ageCalculator = 2023-1987;
+
+//STEP 4
+
+// let firstName;
+// let lastName;
+// let address;
+// let city;
+// let state;
+// let zipCode;
+// let yourAge;
+// let refSource;
+// let mayWeContactYou;
+
+//STEP 5
+
+// let firstName = Jane;
+// let state = CA;
+// let yourAge = 32;
+
+//STEP 6
+// let temp = 22;
+// console.log ('At June 2023, average temperature will be'+ " " + temp +" " + 'Celcius.');
+
+//STEP 7
+// let city = 'San Diego';
+// let southern = true;
+// console.log( `${city} is a Southern city of California.`);
+
+// let qtySeasons = 4;
+// let fourSeasons = true;
+// console.log(`The US has ${qtySeasons} seasons.`);
+
+//STEP 8
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+// window.console.log(someString);
+
+// let answer = 'The above string literal was not valid, to fix it a backslash, BETWEEN I and AM, was added.'
+// window.console.log(answer);
+
+//STEP 9
+// let x = null;
+// window.console.log(x);
+
+// let z;
+// window.console.log(z);
+
+//STEP 10
+// window.console.log(typeof 'San Diego');
+// window.console.log(typeof 4);
+// window.console.log(typeof true);
+
+// let country = {state: 'CA', city: 'San Diego'};
+// window.console.log(typeof country);
+
+// let z;
+// window.console.log(typeof z);
+
+//STEP 11
+// alert('Hello '+'Fernanda, '+'welcome to the JavaScript class!');
+
+//STEP 12
+// let studentName= 'Fernanda';
+// alert('Hello '+ studentName + ', '+'welcome to the JavaScript class!');
+
+//STEP 13
+// let studentName= 'Fernanda';
+// let course = 'Java Script';
+// alert('Hello '+ studentName + ', '+'welcome to the '+ course +' class!');
+
+// STEP 14
+// window.console.log('Hello Zak Ruvalcaba.\nWelcome to the JavaScript class!');
+
+// STEP 15
+// let studentName = prompt('Enter student name');
+// window.console.log('Hello '+ studentName +'\nWelcome to the JavaScript class!');
+
+// STEP 16
+// let studentName = prompt('Enter student name');
+// let course =  prompt('Enter course name');
+// window.console.log('Hello '+ studentName +'\nWelcome to the '+ course + ' class!');
+
+// STEP 17
+// let y = 10;
+// let z = 20;
+// console.log(Number(y) + Number(z));
+
+// STEP 18
+// let x = Number(20);
+// let w = Number(20);
+// let total = x + w;
+// console.log(total);
+
+// STEP 19
+// let x = Number(20);
+// let y = Number(5);
+// let total = x * y;
+// console.log(total);
+
+// STEP 20
+// let x = Number(20%3);
+// let y = Number(1);
+// console.log(x/y);
+
+// STEP 21
+
+// function checkNumber(num) {
+// 	if (num == 8) {
+// 		window.console.log('You guessed the right number!');
+// 	}
+// }
+// checkNumber(8);
+
+
+// STEP 22
+// function guessedNumber(z) {
+// 	if (z != 8) {
+// 		window.console.log('Sorry, you did not guess the right number!');
+// 	}
+// }
+// guessedNumber(9);
